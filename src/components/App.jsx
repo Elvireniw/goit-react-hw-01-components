34e51183ac9c88038a07fb5e4a252c024bc0d8f3
Elvireniw <130295282+Elvireniw@users.noreply.github.com>
2023-12-14 12:21:18 +0000

    profile-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import user from './profile/user.json';
+import { Profile } from './profile/Profile';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </section>
   );
 };
