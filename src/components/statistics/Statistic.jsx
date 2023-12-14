15d048d30f6c5b049caa88396cfb48897e2eb5ff
Elvireniw <130295282+Elvireniw@users.noreply.github.com>
2023-12-14 14:48:29 +0000

    statistics-module.css

--- src/components/statistics/Statistic.jsx
+++ src/components/statistics/Statistic.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import css from './Statistic.module.css'
 
+const getRandomHexColor = idx =>
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+
 export const Statistics = ({ title, stats }) => {
+  const randomColor = {
+    backgroundColor: getRandomHexColor()
+};
   return (
     <section className={css.statistics}>
         {title && <h2 className={css.title}>{title}</h2>}
 
         <ul className={css.statList}>
             {stats.map((item, idx) => (
-                <li key={item.id} id={item.id} index={idx} className={css.item}>
+                <li key={item.id} id={item.id} index={idx} className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
                     <span className={css.label}>{item.label}</span>
                     <span className={css.percentage}>{item.percentage}%</span>
                 </li>
